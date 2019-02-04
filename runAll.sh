a33089ec0a1146e9dc9de85024a1b913c96685a6
Philip Nelson <PhilipNelson5@gmail.com>
2019-02-04 10:21:48 -0700

    colors!

--- runAll.sh
+++ runAll.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
-
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+passed=0;
+tests=0;
 for file in $(find test -type f | grep --invert-match '/lexical/'); do
   printf '━%.0s' $(seq 1 $(tput cols))
   echo ""
-  #len=${#file}
-  #echo "${len}"
-  echo "cpsl < ${file}"
-  ./cpsl < $file
+
+  output=$(./cpsl < ${file} 2>&1)
+
+  if [[ -z ${output} ]]; then
+    echo -e "$GREEN[ PASS ]$RESET cpsl < ${file}"
+    ((++passed))
+  else
+    echo -e "$RED[ FAIL ]$RESET cpsl < ${file}"
+    echo "${output}"
+  fi
+
   echo ""
-  echo ""
+  ((++tests))
 done
+
+echo "Passed ${passed} / ${tests} tests"
--- runTest.sh
+++ runTest.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
 
 file=$(find test -type f | grep --invert-match '/lexical/' | grep $1 | head -n 1)
 echo $file
 printf '━%.0s' $(seq 1 $(tput cols))
 echo ""
+
 cat -n $file
 echo ""
+
 printf '━%.0s' $(seq 1 $(tput cols))
 echo ""
-echo "cpsl < ${file}"
+
+output=$(./cpsl < ${file} 2>&1)
+
+if [[ -z ${output} ]]; then
+  echo -e "$GREEN[ PASS ]$RESET cpsl < ${file}"
+else
+  echo -e "$RED[ FAIL ]$RESET cpsl < ${file}"
+  echo "${output}"
+fi
+
 echo ""
-./cpsl < $file
