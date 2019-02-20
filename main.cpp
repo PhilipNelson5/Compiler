ce4375a710137a3f26e0519adac10b266d859244
Philip Nelson <PhilipNelson5@gmail.com>
2019-02-19 20:02:20 -0700

    sorted commandline option parsing and changed endl to \n in help message

--- main.cpp
+++ main.cpp
@@ -11,24 +11,23 @@ std::shared_ptr<ProgramNode> programNode;
 
 void showHelp()
 {
-  std::cout << std::endl;
-  std::cout << "     ______   _______    ______   __       " << std::endl;
-  std::cout << "    /      \\ |       \\  /      \\ |  \\      " << std::endl;
-  std::cout << "   |  $$$$$$\\| $$$$$$$\\|  $$$$$$\\| $$      " << std::endl;
-  std::cout << "   | $$   \\$$| $$__/ $$| $$___\\$$| $$      " << std::endl;
-  std::cout << "   | $$      | $$    $$ \\$$    \\ | $$      " << std::endl;
-  std::cout << "   | $$   __ | $$$$$$$  _\\$$$$$$\\| $$      " << std::endl;
-  std::cout << "   | $$__/  \\| $$      |  \\__| $$| $$_____ " << std::endl;
-  std::cout << "    \\$$    $$| $$       \\$$    $$| $$     \\" << std::endl;
-  std::cout << "     \\$$$$$$  \\$$        \\$$$$$$  \\$$$$$$$$" << std::endl
-            << std::endl;
+  std::cout << "\n";
+  std::cout << "     ______   _______    ______   __\n";
+  std::cout << "    /      \\ |       \\  /      \\ |  \\\n";
+  std::cout << "   |  $$$$$$\\| $$$$$$$\\|  $$$$$$\\| $$\n";
+  std::cout << "   | $$   \\$$| $$__/ $$| $$___\\$$| $$\n";
+  std::cout << "   | $$      | $$    $$ \\$$    \\ | $$\n";
+  std::cout << "   | $$   __ | $$$$$$$  _\\$$$$$$\\| $$\n";
+  std::cout << "   | $$__/  \\| $$      |  \\__| $$| $$_____\n";
+  std::cout << "    \\$$    $$| $$       \\$$    $$| $$     \\\n";
+  std::cout << "     \\$$$$$$  \\$$        \\$$$$$$  \\$$$$$$$$\n\n";
 
-  std::cout << "-d\t--debug\t\tset yydebug to a positive value" << std::endl;
-  std::cout << "-h\t--help\t\tshow this help message" << std::endl;
-  std::cout << "-p\t--parse-only\tskip all assembly or source code emission"
-            << std::endl;
-  std::cout << "-s\t--source\tparse and emit source code" << std::endl;
-  std::cout << "-v\t--verbose\textra print statements" << std::endl;
+  std::cout << "-d\t--debug\t\tset yydebug to a positive value\n";
+  std::cout << "-h\t--help\t\tshow this help message\n";
+  std::cout << "-p\t--parse-only\tskip all assembly or source code emission\n";
+  std::cout << "-s\t--source\tparse and emit source code\n";
+  std::cout << "-v\t--verbose\textra print statements\n";
+  std::cout << std::endl;
 }
 
 int main(int argc, char** argv)
@@ -45,14 +44,14 @@ int main(int argc, char** argv)
       showHelp();
       return EXIT_SUCCESS;
     }
+    if (strcmp(argv[i], "-d") == 0 | strcmp(argv[i], "--debug") == 0)
+      yydebug = 1;
+    if (strcmp(argv[i], "-p") == 0 | strcmp(argv[i], "--parse-only") == 0)
+      f_parseOnly = true;
     if (strcmp(argv[i], "-s") == 0 | strcmp(argv[i], "--source") == 0)
       f_source = true;
     if (strcmp(argv[i], "-v") == 0 | strcmp(argv[i], "--verbose") == 0)
       f_verbose = true;
-    if (strcmp(argv[i], "-p") == 0 | strcmp(argv[i], "--parse-only") == 0)
-      f_parseOnly = true;
-    if (strcmp(argv[i], "-d") == 0 | strcmp(argv[i], "--debug") == 0)
-      yydebug = 1;
   }
 
   if (f_verbose) std::cout << "Parsing\n" << std::endl;
